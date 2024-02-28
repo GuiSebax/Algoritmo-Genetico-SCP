c0f332ed28f564199f6a6e28015336018f23d54c
Marcos <marcosoliveira159@outlook.com>
2024-02-28 11:05:26 -0300

    Revert "testes sem local search adicionados (apenas o .bat, necessário rodar)"
    This reverts commit 2b736d53916bfa15e838fad3265953ee294a3f5f.

--- run_tests100noLocalSearch.bat
+++
@@ -1,8 +0,0 @@
-python AG.py ./entradas/teste_01.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/teste_02.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/teste_03.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/teste_04.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/wren_01.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/wren_02.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/wren_03.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt &
-python AG.py ./entradas/wren_04.dat 100 100 0.9 0.9 >> ./resultados/output100nls.txt
